14228dc644f47c4be699190158ef5a66046b2728
Rob Nanola <robnanola@gmail.com>
2022-02-06 00:40:48 +0800

    added user nft list, fixed minor bugs

--- ZunaNFT.js
+++ ZunaNFT.js
@@ -2,8 +2,8 @@
 class ZunaNFT {
 
   init() {
-    this._generate('000063169218f348dc640d171b000208934b5a90189038cb3084624a50f7316c', '');
-    this._generate('00005a13429085339c6521ef0300011c82438c628cc431a63298e3721f772d29', '');
+    this._generate('000063169218f348dc640d171b000208934b5a90189038cb3084624a50f7316c', '', tx.publisher);
+    this._generate('00005a13429085339c6521ef0300011c82438c628cc431a63298e3721f772d29', '', tx.publisher);
   }
 
   _get(k, d, parse) {
@@ -31,6 +31,26 @@ class ZunaNFT {
     }
   }
 
+  _mapGet(k, f, d, parse) {
+    const val = storage.mapGet(k, f);
+    if (val === null || val === "") {
+        return d;
+    }
+    if(parse === false){
+      return val;
+    }else{
+      return JSON.parse(val);
+    }   
+  }
+
+  _mapPut(k, f, v, p, stringify) {
+    if(stringify === false){
+      storage.mapPut(k, f, v, p);
+    }else{
+      storage.mapPut(k, f, JSON.stringify(v), p);
+    }
+  }
+
   _delete(k) {
     storage.del(k);
   }
@@ -43,6 +63,10 @@ class ZunaNFT {
 
   can_update(data) {
     return blockchain.requireAuth(blockchain.contractOwner(), "active") && !this.isLocked();
+  }
+
+  _getNow(){
+    return Math.floor(block.time / 1e9)
   }
 
   isLocked() {
@@ -76,58 +100,73 @@ class ZunaNFT {
     }
   }
 
-  _generate(gene, meta) {
-    console.log('>>>>>>>>>>>')
-    console.log('>>>>>>>>>>>1')
+  _addToTokenList(tokenId, user) {
+    let tokenList = this._mapGet('userNFT', user, []);
+    tokenList.push(tokenId);
+    this._mapPut('userNFT', user, tokenList)
+  }
+
+  _removeToTokenList(tokenId, user) {
+    let tokenList = this._mapGet('userNFT', user, []);
+    let idx = tokenList.indexOf(tokenId);
+    if (idx !== -1) {
+      tokenList.splice(idx, 1);
+    }
+    this._mapPut('userNFT', user, tokenList)
+  }
+
+  _updateTokenList(tokenId, userFrom, userTo) {
+    this._removeToTokenList(tokenId, userFrom);
+    this._addToTokenList(tokenId, userTo);
+  }
+
+  _generate(gene, meta, owner) {
     if(gene === undefined) {
         throw 'Invalid gene';
     }
-    console.log('>>>>>>>>>>>2')
-
-    let owner = blockchain.contractOwner();
     let currentID = this._generateID();
-
+    // NFT generation 
     let tokenInfo = {
         owner: owner,
         id: currentID,
         gene: gene,
         meta: meta
     }
-    console.log('>>>>>>>>>>>3')
 
     this._put('zun.' + currentID, owner);
     this._put('znft.' + currentID, tokenInfo, true);
 
     let balance = this._get('bal.' + owner);
     this._put('bal.' + owner, balance + 1);
+    this._addToTokenList(currentID, owner);
     return currentID;
   }
 
   _transferToken(from, to, tokenId) {
+    tokenId = +tokenId;
     let balance_from = this._get('bal.' + from);
     this._put('bal.' + from, balance_from - 1);
 
     let balance_to = this._get('bal.' + to);
     this._put('bal.' + to, balance_to + 1);
-
     this._put('zun.' + tokenId, to);
+    this._delete('app.' + tokenId);
+    this._updateTokenList(tokenId, from, to);
 
-    this._remove('app.' + tokenId);
     const message = "transfer " + tokenId + " to " + to + " from " + from;
     blockchain.receipt(message);
   }
 
 
-  transfer(token, from, to, tokenId) {
-    this._isEqual(token, 'znft', "Invalid token");
-
+  transfer(tokenId, from, to, amount, memo) {
+    // amount is unusable
     let owner = this._get('zun.' + tokenId);
-    this._isNotEqual(to, owner, "Not allowed.");
+    this._isNotEqual(to, owner, "Token belongs to you.");
 
     if (blockchain.requireAuth(from, "active")) {
       this._isEqual(from, owner, "Not allowed");
-      this._transferEx(from, to, tokenId);
-      return
+      this._transferToken(from, to, tokenId);
+      return;
     }
 
     if (blockchain.requireAuth(blockchain.contractOwner(), "active")) {
@@ -151,8 +190,7 @@ class ZunaNFT {
     this._put('app.' + tokenId, to);
   }
 
-  approve(token, from, to, tokenId) {
-    this._mustEqual(token, 'znft', "Token not exist");
+  approve(to, tokenId) {
     this._approveToken(to, tokenId);
   }
 
